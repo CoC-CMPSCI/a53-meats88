5b60347843ec5f419568841705a005268bedf1a3
meats88 <pcx883mi@gmail.com>
2026-04-01 18:35:06 -0700

    done

--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main()
 {
     int i, begin, end, sum = 0;
-
-    // TODO: prompt user for begin and end values
+do{
+    cout << "enter begin and end values:";
+    cin >> begin >> end;
     // TODO: loop from begin to end and accumulate sum of even values
+} while(begin > end);
+for (i = begin; i <= end; i++){
+    if(i % 2 == 0){
+        sum += i;
+    }
+}
+cout << "The sum of even value is " << sum << endl;
+return 0;
 }
